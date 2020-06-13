eb2f6fab5b05c3665aba4ea67ca0d77953ce9f0b
Piotr <s21911@pjwstk.edu.pl>
2020-06-13 14:26:10 +0200

    first part

---
+++ src/main/java/exam2/Product.java
@@ -0,0 +1,8 @@
+package exam2;
+
+public class Product {
+    public int productId;
+    public String name;
+    public int price;
+//Produktposiada pola id, nazwa i cena
+}
---
+++ src/main/java/exam2/Shop.java
@@ -0,0 +1,12 @@
+package exam2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+    public int shopId;
+    public String name;
+    List<Product> productList = new ArrayList<> ();
+    List<Product> soldProduct = new ArrayList<>();
+
+}
