a455a5a10a39982ac447be774e4271855dd44e2c
Piotr <s21911@pjwstk.edu.pl>
2020-06-13 15:02:44 +0200

    second part validation

--- src/main/java/exam2/Shop.java
+++ src/main/java/exam2/Shop.java
@@ -1,12 +1,19 @@
 package exam2;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Shop {
+
+    @Length(min=6, max=6, message="Sorry wrong name")
+    @Pattern(regexp="[0-9]+")
     public int shopId;
     public String name;
     List<Product> productList = new ArrayList<> ();
     List<Product> soldProduct = new ArrayList<>();
+
 
 }
